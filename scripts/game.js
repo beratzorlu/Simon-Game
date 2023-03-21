1a4899c96b3379e0760460c2f09a3f17aa557268
berat.zorlu <berat.zorlu@hotmail.com>
2023-03-21 19:54:39 +0000

    Add JEST theory and practice content.

---
+++ scripts/game.js
@@ -0,0 +1,17 @@
+//game
+//newgame
+//addturn
+//showturns
+//lightson
+//playerturn
+//showscore
+//------------------
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: [],
+};
+
+module.exports = { game }; //This is because we will be exporting more than one object from this file.
