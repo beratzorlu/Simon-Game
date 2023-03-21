c50714aa8d4d54504d94d92747c722bbbc1916ef
berat.zorlu <berat.zorlu@hotmail.com>
2023-03-21 19:58:52 +0000

    Add test to check all functions.

--- scripts/game.js
+++ scripts/game.js
@@ -11,7 +11,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
+    choices: ["button1","button2","button3","button4"],
 };
 
 module.exports = { game }; //This is because we will be exporting more than one object from this file.
